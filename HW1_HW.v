73948befb8ac02375f90a4bb399835515e28a3f3
a147852z <71688412+a147852z@users.noreply.github.com>
2025-04-16 17:28:58 +0800

    Update HW1_HW.v

--- HW1_HW.v
+++ HW1_HW.v
@@ -26,3 +26,16 @@ begin
     q <= count;
 
 end Behavioral;
+# User LEDs
+set_property -dict {PACKAGE_PIN U14 IOSTANDARD LVCMOS25} [get_ports {O_LED[7]}]
+set_property -dict {PACKAGE_PIN U19 IOSTANDARD LVCMOS25} [get_ports {O_LED[6]}]
+set_property -dict {PACKAGE_PIN W22 IOSTANDARD LVCMOS25} [get_ports {O_LED[5]}]
+set_property -dict {PACKAGE_PIN V22 IOSTANDARD LVCMOS25} [get_ports {O_LED[4]}]
+set_property -dict {PACKAGE_PIN U21 IOSTANDARD LVCMOS25} [get_ports {O_LED[3]}]
+set_property -dict {PACKAGE_PIN U22 IOSTANDARD LVCMOS25} [get_ports {O_LED[2]}]
+set_property -dict {PACKAGE_PIN T21 IOSTANDARD LVCMOS25} [get_ports {O_LED[1]}]
+set_property -dict {PACKAGE_PIN T22 IOSTANDARD LVCMOS25} [get_ports {O_LED[0]}]
+
+# User DIP Switches
+set_property -dict {PACKAGE_PIN F22 IOSTANDARD LVCMOS25} [get_ports {reset}]
+set_property -dict {PACKAGE_PIN G22 IOSTANDARD LVCMOS25} [get_ports {btn}]
